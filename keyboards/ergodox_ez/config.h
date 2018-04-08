61adbc1e015be4ccfa8a16ac880a228fa8908271
rduenas (Rodrigo Dueñas) <rduenas@groupon.com>
2018-04-08 01:37:53 -0700

    rduenas config

--- keyboards/ergodox_ez/config.h
+++ keyboards/ergodox_ez/config.h
@@ -102,6 +102,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * 5, which is now closer to 10ms, but still plenty according to
  * manufacturer specs.
  */
+#define DEBOUNCE    2
+
+#define PREVENT_STUCK_MODIFIERS
 
 #define USB_MAX_POWER_CONSUMPTION 500
 
---
+++ keyboards/ergodox_ez/keymaps/rduenas/config.h
@@ -0,0 +1,5 @@
+#ifndef QMK_KEYS_PER_SCAN
+#define QMK_KEYS_PER_SCAN 4
+#endif // !QMK_KEYS_PER_SCAN
+
+#define FORCE_NKRO
