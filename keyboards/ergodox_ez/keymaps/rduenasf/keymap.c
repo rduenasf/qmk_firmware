c723530897f8c15d1766f753f9f8368d938ee7ae
rduenas (Rodrigo Dueñas) <rduenas@groupon.com>
2018-04-20 14:40:18 -0700

    remap: swap down and up

--- keyboards/ergodox_ez/keymaps/rduenasf/keymap.c
+++ keyboards/ergodox_ez/keymaps/rduenasf/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_BSPACE,      KC_Y,         KC_U,        KC_I,        KC_O,        KC_P,             KC_GRAVE,
                     KC_H,         KC_J,        KC_K,        KC_L,        KC_SCOLON,        GUI_T(KC_QUOTE),
     KC_TAB,         KC_N,         KC_M,        KC_COMMA,    KC_DOT,      KC_SLASH,         KC_RSPC,
-                                  KC_UP,       KC_DOWN,     KC_LBRACKET, KC_RBRACKET,      KC_RCTRL,
+                                  KC_DOWN,     KC_UP,       KC_LBRACKET, KC_RBRACKET,      KC_RCTRL,
 
     KC_PGDOWN,      KC_PGUP,
     TG(L_HOTS),
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_BSPACE,      KC_Y,         KC_U,        KC_I,        KC_O,        KC_P,             KC_GRAVE,
                     KC_H,         KC_J,        KC_K,        KC_L,        KC_SCOLON,        ALT_T(KC_QUOTE),
     KC_TAB,         KC_N,         KC_M,        KC_COMMA,    KC_DOT,      KC_SLASH,         KC_RSPC,
-                                  KC_UP,       KC_DOWN,     KC_LBRACKET, KC_RBRACKET,      KC_RCTRL,
+                                  KC_DOWN,     KC_UP,       KC_LBRACKET, KC_RBRACKET,      KC_RCTRL,
 
     KC_PGDOWN,      KC_PGUP,
     TG(L_HOTS),
